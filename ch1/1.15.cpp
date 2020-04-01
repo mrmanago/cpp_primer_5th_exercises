e905d35019c11102fd60bb6b69c83568081fa987
mr manago <mrmanago@outlook.com>
2020-04-01 06:16:20 +0200

    Section 1.4.2

---
+++ ch1/1.15.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main( { // missing closing parenthese. Also curly bracket on same line (Just style preference)
+    std::cout << "Read each file." << std::endl:    // Colon instead of semicolon
+    std::cout << Update master. << std::endl;       // Missing quotes around string literal
+    std::cout << "Write new master." /*<<*/ std::endl;
+    return 0    // Missing semicolon
+}
