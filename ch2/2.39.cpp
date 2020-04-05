7ad9cd36845183c4283eb32bc704f4798ca2e7de
mr manago <mrmanago@outlook.com>
2020-04-05 14:36:29 +0200

    Section 2.6.1

---
+++ ch2/2.39.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+//struct Foo { /**/ }
+//error C2628: 'Foo' followed by 'int' is illegal (did you forget a ';'?)
+//error C3874 : return type of 'main' should be 'int' instead of 'Foo'
+
+int main()
+{
+	return 0;
+}
---
+++ ch2/2.40.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+struct Sales_data {
+	std::string bookNo;
+	unsigned unit_sold = 0;
+	double revenue = 0.0;
+};
+
+int main()
+{
+	return 0;
+}
