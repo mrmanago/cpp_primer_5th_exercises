9de73df1979859f4a0bc6e17ccdf517c06d07911
mr manago <mrmanago@outlook.com>
2020-04-09 15:00:29 +0200

    Section 4.7

---
+++ ch04/4.21.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::vector;
+
+int main()
+{
+	vector<int> v = { 0, 1, 2, 3, 4, 5 };
+	
+	for (auto &i : v)
+		i = i % 2 ? i *= 2 : i;
+	for (auto i : v)
+		cout << i << ' ';	
+
+	return 0;
+}
+
---
+++ ch04/4.22.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+	int grade = 80;
+	string finalgrade = (grade < 60) ? "fail" : "pass";
+	
+	finalgrade = (grade > 90) ? "high pass" : (grade >= 60 && grade <= 75) ? "60-75" : (grade < 60) ? "fail" : "pass";
+
+	// I prefer this one even though it's longer but it is more readable.
+	if (grade > 90) {
+		finalgrade = "highpass";
+	}
+	else if (grade >= 60 && grade <= 75) {
+		finalgrade = "60-75";
+	}
+	else if (grade >= 60) {
+		finalgrade = "pass";
+	}
+	else {
+		finalgrade = "fail";
+	}
+
+	cout << finalgrade << endl;
+
+	return 0;
+}
+
