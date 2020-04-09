45bfacd9f65a24629f149b0a762ddd1ee9da1ea0
mr manago <mrmanago@outlook.com>
2020-04-09 07:52:25 +0200

    Section 4.4

---
+++ ch04/4.15.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstddef>
+#include <iterator>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main()
+{
+	
+	double dval; int ival; int *pi;
+	// illegal code. cannot assign int from type int*
+	//dval = ival = pi = 0;
+	dval = ival = 0;
+	pi = 0;
+
+	return 0;
+}
+
